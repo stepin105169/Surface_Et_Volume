5c56eec6ca06e3b3486cd6259efee0b4d306f65d
Karti A Nair <52342562+stepin105169@users.noreply.github.com>
2020-10-06 13:16:23 +0530

    Update sample1.c

--- 03_Implementation/src/sample1.c
+++ 03_Implementation/src/sample1.c
@@ -1,11 +1,11 @@
 #include <sample1.h>
 
-float sphere(float radius){
-    float area = 4*3.14*radius*radius;
+float circle(float radius){
+    float area = 3.14*radius*radius;
     return area;
 }
 
-float cone(float radius, float slantheight){
-    float area = 3.14*radius*slantheight+3.14*radius*radius;
+float square(float side){
+    float area = side*side;
     return area;
 }
