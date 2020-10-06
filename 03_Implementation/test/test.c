45ecb177dc9ef62eb60e86f13d5d224bb4c8399d
Karti A Nair <52342562+stepin105169@users.noreply.github.com>
2020-10-06 12:03:41 +0530

    Update test.c

--- 03_Implementation/test/test.c
+++ 03_Implementation/test/test.c
@@ -1,28 +1,40 @@
-#include <unity.h>
-#include <unity_internals.h>
-#include <sample1.h>
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
 
-	void setUp()
-	{	}
-	void tearDown()
-	{	}
+#define PROJECT_NAME    "Sample"
+#include<sample1.h>
 
-	void test_sphere(void)
+void test_sphere(void);
+void test_cone(void);
+
+int main() {
+
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+  CU_add_test(suite, "sphere", test_sphere);
+  CU_add_test(suite, "cone", test_cone);
+
+
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+
+
+  CU_basic_run_tests();
+
+
+  CU_cleanup_registry();
+
+  return 0;
+}
+
+void test_sphere(void)
 	{
-  		TEST_ASSERT_EQUAL(113.040001, sphere(3));
+	CU_ASSERT(113.040001 == sphere(3));
+  	//TEST_ASSERT_EQUAL(113.040001, sphere(3));
 	}
 
-    void test_cone(void)
+void test_cone(void)
 	{
-        TEST_ASSERT_EQUAL(25.120001, cone(2,2));
-	}
-
-	int test_main(void)
-	{
-	  	UNITY_BEGIN();
-	  	RUN_TEST(test_sphere);
-	  	RUN_TEST(test_cone);
-
-
-  		return UNITY_END();
+	CU_ASSERT(25.120001 == cone(2,2));
+        //TEST_ASSERT_EQUAL(25.120001, cone(2,2));
 	}
