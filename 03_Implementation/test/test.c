63fa038aebab9de24e4ca46207c93696ee0538e1
Karti A Nair <52342562+stepin105169@users.noreply.github.com>
2020-10-06 13:21:16 +0530

    Update test.c

--- 03_Implementation/test/test.c
+++ 03_Implementation/test/test.c
@@ -4,16 +4,16 @@
 #define PROJECT_NAME    "Sample"
 #include<sample1.h>
 
-void test_sphere(void);
-void test_cone(void);
+void test_circle(void);
+void test_square(void);
 
 int main() {
 
   if (CUE_SUCCESS != CU_initialize_registry())
     return CU_get_error();
   CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
-  CU_add_test(suite, "sphere", test_sphere);
-  CU_add_test(suite, "cone", test_cone);
+  CU_add_test(suite, "circle", test_circle);
+  CU_add_test(suite, "square", test_square);
 
 
   CU_basic_set_mode(CU_BRM_VERBOSE);
@@ -27,14 +27,14 @@ int main() {
   return 0;
 }
 
-void test_sphere(void)
+void test_circle(void)
 	{
-	CU_ASSERT(113.040001 == sphere(3));
+	CU_ASSERT(50.240002 == circle(4));
   	//TEST_ASSERT_EQUAL(113.040001, sphere(3));
 	}
 
-void test_cone(void)
+void test_square(void)
 	{
-	CU_ASSERT(25.120001 == cone(2,2));
+	CU_ASSERT(36.000000 == square(6));
         //TEST_ASSERT_EQUAL(25.120001, cone(2,2));
 	}
