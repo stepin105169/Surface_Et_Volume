178a5223b2991346f91720ca2a2415ac679c7c1b
Karti A Nair <52342562+stepin105169@users.noreply.github.com>
2020-11-06 11:01:32 +0530

    Update sample1.h

--- 03_Implementation/inc/sample1.h
+++ 03_Implementation/inc/sample1.h
@@ -4,10 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float circle(float radius);
-float square(float side);
-float rectangle(float length, float width);
-float triangle(float base, float height);
+float circle(float radius);        //circle 
+
+float square(float side);          //square
+
+float rectangle(float length, float width);     //rectangle
+
+float triangle(float base, float height);       //triangle
 
 #endif // SAMPLE1_H_INCLUDED
- 
