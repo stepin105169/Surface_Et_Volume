052c743976c58fac6636915d32c98e0bf40ed593
Karti A Nair <52342562+stepin105169@users.noreply.github.com>
2020-11-06 11:03:57 +0530

    Update sample1.c

--- 03_Implementation/src/sample1.c
+++ 03_Implementation/src/sample1.c
@@ -1,21 +1,21 @@
 #include <sample1.h>
 
 float circle(float radius){
-    float area = 3.14*radius*radius;
+    float area = 3.14*radius*radius;        //area of circle
     return area;
 }
 
 float square(float side){
-    float area = side*side;
+    float area = side*side;                 //area of square
     return area;
 }
 
 float rectangle(float length, float width){
-    float area = length*width;
+    float area = length*width;              //area of rectangle
     return area;
 }
 
 float triangle(float base, float height){
-    float area = (base*height)/2;
+    float area = (base*height)/2;           //area of triangle
     return area;
 }
